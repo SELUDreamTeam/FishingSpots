6b6619f18ea8e708832647dcd0a1e452ae82c20e
billy147 <wewill147@gmail.com>
2016-10-03 23:45:56 -0500

    new files

--- FishingSpots/settings.py
+++ FishingSpots/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.gis',
     'rest_framework',
     'rest_framework_gis',
+    'rigolets',
 ]
 
 REST_FRAMEWORK = {
--- rigolets/views.py
+++ rigolets/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
 
 # Create your views here.
 def index (request):
-	return HttpResponse("Hey there, Rigolets!")
+	template = loader.get_template('rigolets/index.html')
+	context = {'hey' : 'Rigolets'}
+	return HttpResponse(template.render(context, request))
